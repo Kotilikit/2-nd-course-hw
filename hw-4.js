d78b9bdf39db7e4b24b76366ceba813e81c37a9e
Даниил Кит <kotilikit@yandex.ru>
2023-10-16 00:17:28 +0300

    4 ДЗ

---
+++ hw-4.js
@@ -0,0 +1,52 @@
+for (let i = 0; i < 2; i++) {
+    console.log("Привет");
+}
+// Задание 1
+
+let i = 1;
+
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+// Задание 2
+
+for (let i = 7; i <= 22; i++) {
+    console.log(i);
+}
+// Задание 3
+
+let obj = {
+    "Коля" : '200',
+    "Вася" : '300',
+    "Петя" : '400'
+};
+
+for (let key in obj) {
+    console.log(`${key} - зарплата ${obj[key]}`);
+}
+// Задание 4
+
+let n = 1000;
+let num = 0;
+
+while (n >= 50) {
+    n = n / 2;
+    num++;
+}
+
+console.log("Полученное число:", n);
+console.log("Количество итераций:", num);
+// Задание 5
+
+let fri = 5;
+
+for (let day = 1; day <= 31; day++) {
+    if ((day - 1) % 7 === fri - 1) {
+        console.log(`Сегодня пятница, ${day}-е число. Необходимо подготовить отчет`);
+    }
+}
+// Задание 6
+
+
+
